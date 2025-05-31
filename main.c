411fd107b12d7310f59ba3fcf02417bdd533f5e4
AsahiTokunaga <hihimamug14@gmail.com>
2025-06-01 01:08:43 +0900

    short

--- main.c
+++ main.c
@@ -2,74 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-void shi();
-void ka();
-void no();
-void ko();
-void ta();
-void nn();
+int c=20;
+char *s[]={"し","か","の","こ","た","ん"};
 
-int count = 20;
-
-int main(void)
-{
-  srand(time(NULL));
-  shi();
-  printf("\n");
-  return 0;
-}
-
-void shi()
-{
-  if (count > 0)
-  {
-    printf("し");
-    if (rand() & 1)
-      ka();
-    else
-      ta();
+void f(int i){
+  if(!c&&!i)return;
+  printf("%s",s[i]);
+  if(i==5){
+    if(rand()&1)f(4);else{c--;printf("\n");f(0);}
+    return;
   }
-}
-
-void ka()
-{
-  printf("か");
-  no();
-}
-
-void no()
-{
-  printf("の");
-  ko();
-}
-
-void ko()
-{
-  printf("こ");
-  int v = rand() % 4;
-  if (v == 0)
-    shi();
-  else if (v == 1)
-    ko();
-  else
-    no();
-}
-
-void ta()
-{
-  printf("た");
-  nn();
-}
-
-void nn()
-{
-  printf("ん");
-  if (rand() & 1)
-    ta();
-  else
-  {
-    count--;
-    printf("\n");
-    shi();
+  if(i==3){
+    int v=rand()%4;
+    f(v<2?v==0?0:3:2);
+    return;
   }
+  f(i==0?rand()&1?1:4:i==1?2:i==2?3:5);
 }
+
+int main(){srand(time(0));f(0);return 0;}
