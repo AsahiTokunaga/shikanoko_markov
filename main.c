9c1f9c592c42cc51de499b76a43a73e50bbe6c3c
AsahiTokunaga <hihimamug14@gmail.com>
2025-06-01 11:56:25 +0900

    improved randomness

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/random.h>
 
 int c=0;
 char *s[]={"し","か","の","こ","た","ん"};
+
+unsigned int get_seed() {
+  unsigned int seed;
+  ssize_t result;
+  result=getrandom(&seed,sizeof(seed),0);
+  if(result<0){
+    perror("getrandom failed, using time() instead\n");
+    seed=(unsigned int)time(0);
+  }
+  return seed;
+}
 
 void f(int i){
   if(!c&&!i)return;
@@ -20,4 +32,4 @@ void f(int i){
   f(i==0?rand()&1?1:4:i==1?2:i==2?3:5);
 }
 
-int main(int argc, char** argv){if(argc<2){printf("Frequency not found. (e.g. ./main 10)\n");return 1;}srand(time(0));c=atoi(argv[1]);f(0);return 0;}
+int main(int argc, char** argv){if(argc<2){printf("Frequency not found. (e.g. ./main 10)\n");return 1;}srand(get_seed());c=atoi(argv[1]);f(0);return 0;}
